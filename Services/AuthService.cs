b72cff774f0ac1e362b4897e9c1c0efaaebc7fa0
상현 <91441630+csh6779@users.noreply.github.com>
2025-08-17 21:16:21 +0900

    API 추가
    사용자 조회 / 수정 / 삭제 구현
    DB 연동

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -14,6 +14,7 @@ namespace GamjaTest.Services
             _context = context;
         }
 
+        //회원가입
         public async Task<UserResponseDto> RegisterUserAsync(UserRequestDto request)
         {
             if (await _context.Users.AnyAsync(u => u.Email == request.Email))
@@ -39,6 +40,69 @@ namespace GamjaTest.Services
                 Name = user.Name,
                 Email = user.Email,
             };
+        }
+
+        // 전체 조회
+        public async Task<List<UserResponseDto>> GetAllUsersAsync()
+        {
+            return await _context
+                .Users.Select(u => new UserResponseDto
+                {
+                    Id = u.Id,
+                    Name = u.Name,
+                    Email = u.Email,
+                })
+                .ToListAsync();
+        }
+
+        //단건 조회
+        public async Task<UserResponseDto?> GetUserByIdAsync(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+                return null;
+
+            return new UserResponseDto
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Email = user.Email,
+            };
+        }
+
+        //정보 수정
+        public async Task<UserResponseDto?> updatedUserAsync(int id, UserRequestDto request)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+                return null;
+
+            user.Name = request.Name;
+            user.Email = request.Email;
+            user.Password = request.Password;
+            user.UpdatedAt = DateTime.UtcNow;
+
+            await _context.SaveChangesAsync();
+
+            return new UserResponseDto
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Email = user.Email,
+            };
+        }
+
+        // 삭제
+        public async Task<bool> DeleteUserAsync(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+                return false;
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return true;
         }
     }
 }
